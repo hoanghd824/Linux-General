eec86b0628535b5579cb6674a0153825871ff60f
Hoang <dhaongho2000@gmail.com>
2022-07-07 20:45:46 +0700

    Update
    Update

--- Shell script/install.sh
+++ Shell script/install.sh
@@ -4,10 +4,6 @@ echo "Update & upgrade"
 apt update -y
 apt upgrade -y
 
-echo "Install MATE desktop environment"
-apt install ubuntu-mate-desktop -y
-apt install mate-desktop-environment-extras -y
-
 echo "Install git"
 apt install git -y
 
@@ -28,24 +24,9 @@ echo "Install Java"
 sudo apt install default-jre
 sudo apt install default-jdk
 
-echo "Install tlp & tlpUI"
-sudo add-apt-repository ppa:linrunner/tlp
-sudo add-apt-repository ppa:linuxuprising/apps
-sudo apt update
-sudo apt install tlp tlptlp-rdw -y
-sudo apt install tlpui -y
-
-sudo add-apt-repository ppa:slimbook/slimbook
-sudo apt update
-sudo apt install slimbookbattery
-
 sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
 sudo apt-get update
 sudo apt-get install ibus ibus-bamboo --install-recommends
 ibus restart
 env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
-#echo "Install Parrot theme"
-#git clone https://github.com/ParrotSec/parrot-themes
-#cd parrot-themes
-#cp -r icons/ themes/  /usr/share 
 
